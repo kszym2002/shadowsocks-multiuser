c8a3a77d12806ae5a0f5386e1a21ba6eafbf6631
kszym2002 <kszym2002@gmail.com>
2019-08-26 17:19:28 +0800

    Update Ubuntu.sh

--- scripts/Ubuntu.sh
+++ scripts/Ubuntu.sh
@@ -7,8 +7,8 @@ rm go.tar.gz
 OLDPATH="$PATH"
 PATH="$PATH:/usr/local/go/bin"
 
-go get -u -v github.com/NetchX/shadowsocks-multiuser
-cd ~/go/src/github.com/NetchX/shadowsocks-multiuser
+go get -u -v github.com/kszym2002/shadowsocks-multiuser
+cd ~/go/src/github.com/kszym2002/shadowsocks-multiuser
 
 go build -ldflags "-w -s"
 
